5b5b25bddaeb0b56c6f60b9599cb7c1af814e008
CesarHerrera <elch.herrera@gmail.com>
2021-10-30 22:53:07 -0500

    Created api for the movieDB

---
+++ src/scripts/api.js
@@ -0,0 +1,18 @@
+const apiConfig = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: "2e24785394d993f26fba2a923499635d",
+    POPULAR_MOVIE_URL: "/movie/popular?api_key="
+}
+
+const apiFunctions = {
+    popularMovies: async()=>{
+        let url = apiConfig.BASE_URL+apiConfig.POPULAR_MOVIE_URL+apiConfig.API_KEY;
+        return await (await fetch(url)).json();
+    },
+    posterUrl: function(movie) {
+        var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
+        return baseImageUrl + movie.poster_path; 
+    }
+
+}
+export default apiFunctions;
