b03b3f431159e2cb4d8086465f8cc6051d55410e
CesarHerrera <elch.herrera@gmail.com>
2021-11-22 21:01:01 -0500

    Overview comments

--- src/scripts/overview.js
+++ src/scripts/overview.js
@@ -7,18 +7,14 @@ import UTIL from './util.js';
 const params = new URLSearchParams(window.location.search);
 const movieId = params.get('movieId');
 
-/**
- * Getting the data from the API
- */
-const movie = (await API.movie(movieId));
-const videosJson = (await API.videos(movieId));
-const video = videosJson.results[0];
-let trailer = null;
-if(video) trailer = video.key;
 
 /**
  * OVERVIEW SECTION
  */
+
+// Getting the data from the API
+const movie = (await API.movie(movieId));
+
 let overviewContainer = document.getElementById('overviewContainer');
 overviewContainer.style.backgroundImage = `url(${API.backgroundUrl(movie)})`;
 // Main Container
@@ -35,12 +31,21 @@ overviewContainer.appendChild(plot);
 /**
  * TRAILER SECTION
  */
+
+// Getting the data from the API
+const videosJson = (await API.videos(movieId));
+const video = videosJson.results[0];
+let trailer = null;
+if(video) trailer = video.key;
+// If the trailer exists we include it
 if(trailer){
     let trailerContainer = document.getElementById('trailerContainer');
+    // Subheader
     let title = document.createElement('div');
     title.className += " subheader";
     title.innerHTML = "Trailer";
     trailerContainer.appendChild(title);
+    // Embedded trailer from youtube
     let videoTrailer = document.createElement('div');
     videoTrailer.className += " trailer";
     videoTrailer.innerHTML = `
@@ -57,6 +62,7 @@ if(trailer){
     `;
     trailerContainer.appendChild(videoTrailer);
 }else{
+    // Some styling if the trailer doesn't exist
     let trailerContainer = document.getElementById('trailerContainer');
     trailerContainer.style = " width: 0;";
     let recommendationsContainer = document.getElementById('recommendationsContainer');
@@ -67,22 +73,28 @@ if(trailer){
 /**
  * RECOMMENDATIONS SECTION
  */
+
+// Getting the data from the API
 let recommendations = (await API.recommendations(movieId));
 let recommendedMovies = recommendations.results;
+// If the trailer exists we only show 3 recommendations, if not we show 5
 if (trailer) recommendedMovies = recommendedMovies.slice(0,3);
 else recommendedMovies = recommendedMovies.slice(0,5);
 
 let recommendationsContainer = document.getElementById('recommendationsContainer');
+// Subheader
 let title = document.createElement('div');
 title.className += " subheader";
 title.innerHTML = "Recomendaciones";
 recommendationsContainer.appendChild(title);
+// If there are no recommendations we show a message
 if(recommendedMovies.length == 0){
     let message = document.createElement("h4");
     message.innerHTML = "No existen recomendaciones"
     message.style = "height: 350px; padding-top: 20px;"
     recommendationsContainer.appendChild(message);
 }else{
+    // Creating the recommendations
     let gridRecommendations = document.createElement('div');
     gridRecommendations.className += " recommendations";
     recommendedMovies.map(movie => {
@@ -94,13 +106,17 @@ if(recommendedMovies.length == 0){
 /**
  * CAST SECTION
  */
+
+// Getting the data from the API
 let credits = (await API.credits(movieId));
 let cast = credits.cast.slice(0,5);
 let castContainer = document.getElementById('castContainer');
+// Subheader
 let title2 = document.createElement('div');
 title2.className += " subheader";
 title2.innerHTML = "Actores Principales";
 castContainer.appendChild(title2);
+// Creating the characters
 let gridCastContainer = document.getElementById('gridCastContainer');
 cast.map(character=>{
     gridCastContainer.append(UTIL.createCharacterThumbnail(character));
